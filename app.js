a76bbd23b46bb69143fac3533c8ec7e01555915f
Nikolay Komolov <nkspb13@gmail.com>
2017-08-30 12:23:56 +0300

    Added routes

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require('express');
 
 const app = express()
-console.log(typeof(app));
-app.listen(3000);
+
+app.get('/', (req, res) => {
+    res.send('<h1>I love Treehouse</h1>');    
+});
+
+app.get('/hello', (req, res) => {
+    res.send('<h1>Hello, JavaScript Developer!</h1>');    
+});
+
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000');
+});
