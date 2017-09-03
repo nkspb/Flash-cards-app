53e73b154665bb200de34dbc02506e3fbc2a4d82
Nikolay <nkspb13@gmail.com>
2017-09-03 18:11:24 +0300

    Added cookies support

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express()
 // html forms normally encode the data the same way urls do
 app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
@@ -12,10 +14,11 @@ app.get('/', (req, res) => {
 });
 
 app.get('/hello', (req, res) => {
-    res.render('hello');
+    res.render('hello', {name: req.cookies.username});
 });
 
 app.post('/hello', (req, res) => {
+    res.cookie('username', req.body.username);
     res.render('hello', { name:req.body.username });
 });
 
