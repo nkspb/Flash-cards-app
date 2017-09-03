92aba47473172146c69852b039d93e7c2367b2c6
Nikolay <nkspb13@gmail.com>
2017-09-03 19:25:20 +0300

    Added redirection

--- app.js
+++ app.js
@@ -10,16 +10,20 @@ app.use(cookieParser());
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.render('index');    
+    const name = req.cookies.username
+    if (name) {
+        res.render('index', { name });   
+    } 
+    res.redirect('/hello');
 });
 
 app.get('/hello', (req, res) => {
-    res.render('hello', {name: req.cookies.username});
+    res.render('hello');
 });
 
 app.post('/hello', (req, res) => {
     res.cookie('username', req.body.username);
-    res.render('hello', { name:req.body.username });
+    res.redirect('/');
 });
 
 app.get('/cards', (req, res) => {
