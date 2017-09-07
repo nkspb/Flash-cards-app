98db92970d3a4d9baa63d27286d2abe0afe5cf22
Nikolay Komolov <nkspb13@gmail.com>
2017-09-07 13:10:55 +0300

    Modularized routes

--- app.js
+++ app.js
@@ -9,50 +9,11 @@ app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
-app.use((req, res, next) => {
-    console.log('Hello');
-    // const err = new Error('Oh noes!');
-    // err.status = 500;
-    next();
-});
+const mainRoutes = require('./routes');
+const cardRoutes = require('./routes/cards');
 
-app.use((req, res, next) => {
-    console.log(' World');
-    next();
-});
-
-app.get('/', (req, res) => {
-    const name = req.cookies.username;
-    if (name) {
-        res.render('index', { name });   
-    } else {
-        res.redirect('/hello');
-    }
-});
-
-app.get('/hello', (req, res) => {
-    const name = req.cookies.username;
-    if (name) {
-        res.redirect('/');    
-    } else {
-        res.render('hello');
-    }
-});
-
-app.post('/hello', (req, res) => {
-    res.cookie('username', req.body.username);
-    res.redirect('/');
-});
-
-// goodbye button route
-app.post('/goodbye', (req, res) => {
-    res.clearCookie('username');    
-    res.redirect('/hello');
-});
-
-app.get('/cards', (req, res) => {
-    res.render('card', {prompt: "Who is buried in Grant't tomb?"});    
-});
+app.use(mainRoutes);
+app.use('/cards', cardRoutes);
 
 app.use((req, res, next) => {
     const err = new Error('Not Found');
