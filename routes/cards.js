ae6b9f3ab7e132fcecc2b20f9535b95f60c069d5
Nikolay Komolov <nkspb13@gmail.com>
2017-09-08 14:01:16 +0300

    Added next card button, fixed incorrect side requests

--- routes/cards.js
+++ routes/cards.js
@@ -3,14 +3,37 @@ const router = express.Router();
 const { data } = require('../data/flashcardData.json');
 const { cards } = data;
 
+router.get('/', (req, res) => {
+    const numberOfCards = cards.length;
+    const flashcardId = Math.floor(Math.random() * numberOfCards)
+    res.redirect(`/cards/${flashcardId}`)
+});
+
 router.get('/:id', (req, res) => {
     const { side } = req.query;
     const { id } = req.params;
+
+    if (!side) {
+        res.redirect(`/cards/${id}?side=question`)
+    }
+
+    const name = req.cookies.username;
     const text = cards[id][side];
     const { hint } = cards[id];
 
-    const templateData = { text, hint };
-
+    const templateData = { id, text, name };
+    
+    if (side === 'question') {
+        templateData.hint = hint;
+        templateData.sideToShow = 'answer';
+        templateData.sideToShowDisplay = 'Answer';
+    } else if (side === 'answer') {
+        templateData.sideToShow = 'question';   
+        templateData.sideToShowDisplay = 'Question'; 
+    } else {
+        // if user enteres incorrect side
+        res.redirect(`/cards/${id}`);
+    }
     res.render('card', templateData);    
 });
 
